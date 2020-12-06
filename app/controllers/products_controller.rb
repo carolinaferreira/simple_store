22bda2fbd524e829af935840c309277b4bfe195c
carolinaferreira <carol.mf14@gmail.com>
2020-12-06 18:21:44 -0300

    added function to create new products :sparkles:

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
         @products = Product.order :name
         @product_with_disccount = Product.order(:cost).limit 1
     end
+
+    def create
+        product = params.require(:product).permit(:name, :description, :cost, :quantity)
+        Product.create product
+        redirect_to root_path
+    end
 end
