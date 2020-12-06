f73912b608bb3dc053c3d374713716bd027c2634
carolinaferreira <carol.mf14@gmail.com>
2020-12-06 15:12:40 -0300

    showing product with lowest price

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.order :name
+        @product_with_disccount = Product.order(:cost).limit 1
+    end
 end
